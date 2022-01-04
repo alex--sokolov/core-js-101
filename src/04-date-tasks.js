db5bd6a3f23a8c73e3b19634f8958814b76d2243
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-01-04 22:56:14 +0300

    feat: add date

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,10 +53,11 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const dateMy = new Date(date);
+  const year = dateMy.getFullYear();
+  return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
 }
-
 
 /**
  * Returns the string representation of the timespan between two dates.
@@ -73,10 +74,15 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
-}
 
+function timeSpanToString(startDate, endDate) {
+  const res = endDate.getTime() - startDate.getTime();
+  const hours = (Math.floor(res / 60 / 60 / 1000)).toString().padStart(2, '0');
+  const mins = (Math.floor(res / 60 / 1000) % 60).toString().padStart(2, '0');
+  const secs = (Math.floor(res / 1000) % 60).toString().padStart(2, '0');
+  const ms = Math.floor(res % 1000).toString().padStart(3, '0');
+  return `${hours}:${mins}:${secs}.${ms}`;
+}
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
@@ -94,10 +100,16 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
-}
 
+function angleBetweenClockHands(date) {
+  let hours = date.getUTCHours();
+  if (hours >= 12) hours -= 12;
+  const mins = date.getUTCMinutes();
+  const degMins = (360 / 60) * mins;
+  const degHours = (360 / 12) * hours + degMins / 12;
+  const degDiff = Math.abs(degHours - degMins);
+  return (Math.min(degDiff, 360 - degDiff) * Math.PI) / 180;
+}
 
 module.exports = {
   parseDataFromRfc2822,
